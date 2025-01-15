8329b3f35bdf56be30654dfcdeef2b12a4800201
adrianbanciu01 <banciu01@gmail.com>
2025-01-15 23:52:08 +0200

    Add project files.

---
+++ BanciuAdrianLab11/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using BanciuAdrianLab11.Data;
+
+
+namespace BanciuAdrianLab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
